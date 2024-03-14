1ee2a1aca5bf7416b77e363a1cc6e5656ffefd8e
CRYcombat <koteika5063@gmail.com>
2024-03-14 17:49:25 +0400

    First task

---
+++ Football.DAL/Entities/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football.DAL.Entities
+{
+	public class Team
+	{
+		public int Id { get; set; }
+		
+		public string Name { get; set; }
+		public string City { get; set; }
+		public int Victories { get; set; }
+		public int Looses { get; set; }
+		public int Draws { get; set; }
+	}
+}
