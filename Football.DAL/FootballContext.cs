e2cf6b9dd6a7e18586d57650b0d293268b283fbb
CRYcombat <koteika5063@gmail.com>
2024-03-14 17:51:44 +0400

    Second task

---
+++ Football.DAL/FootballContext.cs
@@ -0,0 +1,16 @@
+﻿using Football.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Football.DAL
+{
+	public class FootballContext : DbContext
+	{
+		private string _connectionString => "Data Source=CRYCOMBATPC\\MSSQL2019;Initial Catalog=Football;Integrated Security=True;Connect Timeout=30;Trust Server Certificate=True;";
+
+		public DbSet<Team> Teams { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+			optionsBuilder.UseSqlServer(_connectionString);
+		}
+	}
+}
